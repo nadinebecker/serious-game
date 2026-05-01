bca7c84a955c383d96651c847194fb49c02d8b17
Nadine Becker <nadine.becker1@iu-study.org>
2026-05-01 12:05:51 +0200

    feat: add initial content to index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Startwerte
+let level = 1;
+let points = 0;
+
+// Werte in die HTML-Elemente schreiben
+document.getElementById("level").textContent = level;
+document.getElementById("points").textContent = points;
