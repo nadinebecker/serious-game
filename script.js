16572d9239cddfbdfb12c99aad9be86b63905a68
Nadine Becker <nadine.becker1@iu-study.org>
2026-05-03 15:31:50 +0200

    feat: load posts from posts.json and display first post

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // Startwerte
 let level = 1;
 let points = 0;
@@ -5,3 +6,25 @@ let points = 0;
 // Werte in die HTML-Elemente schreiben
 document.getElementById("level").textContent = level;
 document.getElementById("points").textContent = points;
+
+
+// Posts laden
+fetch("posts.json")
+  .then(response => response.json())
+  .then(posts => {
+    console.log("Posts geladen:", posts);
+
+   
+    showPost(posts[0]);
+  })
+  .catch(error => {
+    console.error("Fehler beim Laden der Posts:", error);
+  });
+
+function showPost(post) {
+  console.log("Aktueller Post:", post);
+ document.getElementById("postAuthor").textContent = post.author;
+  document.getElementById("postContent").textContent = post.content;
+  document.getElementById("postSource").textContent = "Quelle: " + post.source;
+ 
+}
